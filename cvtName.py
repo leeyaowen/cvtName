6861f0c3b688e9271605351cac01acbfa05b3e98
leeyaowen <jcjh30101@gmail.com>
2020-09-11 09:09:28 +0800

    try讀取 / 增加時間計算

--- cvtName.py
+++ cvtName.py
@@ -2,6 +2,7 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 import glob
 import os
+import timeit
 
 
 def rename():
@@ -13,21 +14,31 @@ def rename():
     plot = input('plot=?\n')
     i = 0
 
+    start = timeit.default_timer()
+    print('please wait...')
+
     for filename in all_file:
         i = i+1
         metadata = {}
-        with Image.open(filename) as img:
-            info = img._getexif()
-        for tag, value in info.items():
-            tagname = TAGS.get(tag, tag)
-            metadata[tagname] = value
+        # noinspection PyBroadException
+        try:
+            with Image.open(filename) as img:
+                info = img._getexif()
+            for tag, value in info.items():
+                tagname = TAGS.get(tag, tag)
+                metadata[tagname] = value
+            camtime = metadata['DateTimeOriginal']
+            datetime = str('%s-%s-%s-%s' % (camtime[0:4], camtime[5:7], camtime[8:10], camtime[11:13]))
 
-        camtime = metadata['DateTimeOriginal']
-        datetime = str('%s-%s-%s-%s' % (camtime[0:4], camtime[5:7], camtime[8:10], camtime[11:13]))
+            os.rename(filename, '%s/%s-%s_%s.jpg' % (path, plot, datetime, str(i)))
+        except Exception:
+            print('error in %s\n' % filename)
+            continue
 
-        os.rename(filename, '%s/%s-%s_%s.jpg' % (path, plot, datetime, str(i)))
+    stop = timeit.default_timer()
 
-    print('Complete!\n')
+    print('Complete!')
+    print('time : %s\n' % str(stop-start))
 
 
 while __name__ == '__main__':
